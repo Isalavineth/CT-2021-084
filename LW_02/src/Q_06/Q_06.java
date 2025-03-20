82568410a2f3698bf0b36d032b083cf03e0a0039
Isala Vineth <isalavineth@gmail.com>
2025-03-20 17:46:06 +0530

    Update Q_06.java

--- LW_02/src/Q_06/Q_06.java
+++ LW_02/src/Q_06/Q_06.java
@@ -1,13 +1,15 @@
 package Q_06;
 
+import java.time.Year;
 import java.util.Scanner;
 
 public class Q_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Year currentYear = Year.now();
         int year;
-        System.out.print("Enter your birth year: ");
+        System.out.print("Enter the year you were born: ");
         year = scanner.nextInt();
-        System.out.print("You were born in "+year+" and will be(are) "+(2025-year)+" this year.");
+        System.out.print("You were born in "+year+" and will be(are) "+(currentYear.getValue()-year)+" this year.");
     }
 }
